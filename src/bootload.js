b7616ee7e72929610e93cc355a68229bba99554b
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2023-10-15 17:43:29 -0300

    scene creation - bootloader

---
+++ src/bootload.js
@@ -0,0 +1,33 @@
+import { Scene } from 'phaser'
+import { Overworld } from './scenes/Overworld'
+
+export class Bootload extends Scene {
+	constructor() {
+		super({ key: 'bootload' })
+	}
+
+	preload() {
+		this.load.path = 'src/assets/'
+
+		// PLAYER
+		this.load.spritesheet('player', 'character/character.png', {
+			frameWidth: 50,
+			frameHeight: 37,
+		})
+
+		// ENEMIES
+		//  - Shaddy
+		this.load.json('shadAnim', 'enemies/shaddy/shaddy_anim.json')
+		this.load.atlas(
+			'shaddy',
+			'enemies/shaddy/shaddy.png',
+			'enemies/shaddy/shaddy_atlas.json'
+		)
+	}
+
+	create() {
+		console.log('resources loaded.')
+		this.scene.add('Overworld', new Overworld()) // <- scene creation
+		this.scene.start('Overworld') // <- play scene
+	}
+}
